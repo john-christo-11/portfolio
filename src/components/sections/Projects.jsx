4385dc168a3412f14fca9af8966f7490b1960614
John Christopher <johnchristopher07849@gmail.com>
2025-02-18 21:26:21 +0530

    Add files via upload

---
+++ src/components/sections/Projects.jsx
@@ -0,0 +1,165 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const Projects = () => {
+  return (
+    <section
+      id="projects"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="max-w-5xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            {" "}
+            Featured Projects
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2"> Edukits</h3>
+              <p className="text-gray-400 mb-4">
+              EduKits is a mobile app designed for students to easily share and access study materials, fostering collaborative learning. It provides a seamless platform for exchanging notes, resources, and academic content.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Java", "Xml", "Firebase"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  
+                </a>
+              </div>
+            </div>
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Portfolio</h3>
+              <p className="text-gray-400 mb-4">
+              This portfolio is built using React, Tailwind CSS, and Vite, ensuring a fast, responsive, and modern user experience. It showcases my skills, projects, and expertise in front-end development with a sleek and efficient design.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React", "Tailwind", "Vite"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Shopping Cart System</h3>
+              <p className="text-gray-400 mb-4">
+              Developed a responsive web application to manage shopping lists with item 
+              addition, updates, and deletion. Added dynamic features like cart calculations and a search functionality. 
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Java","HTML", "Css", "JavaScript", "SQL"].map(
+                  (tech) => (
+                    <span
+                      key={tech}
+                      className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/john-christo-11/Project2"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  github →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">Medical Prescription System </h3>
+              <p className="text-gray-400 mb-4">
+              Designed a web-based system to manage patient prescriptions with real-time 
+              notifications. Implemented secure login and data encryption for patient records.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Java","HTML", "Css", "JavaScript", "SQL"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center ">
+                <a
+                  href="https://github.com/john-christo-11/Project1"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                   github →
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
